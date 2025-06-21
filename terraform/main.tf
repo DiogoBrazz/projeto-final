05183e084d9ca88604785be2fc230a19d81da4be
Diogo Braz <diogopappa@hotmail.com>
2025-06-21 16:18:57 -0300

    fix(terraform): Muda tipo de disco para pd-standard para evitar erro de cota

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,10 +27,11 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "default-pool"
   location   = "us-central1-a"
   cluster    = google_container_cluster.primary.name
-  node_count = 2 # Começamos com 2 nós
+  node_count = 2
 
   node_config {
     machine_type = "e2-small"
+    disk_type    = "pd-standard" 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
