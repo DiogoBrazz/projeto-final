981c4e97726758b55e442eb6cf758bf17401eb05
Diogo Braz <diogopappa@hotmail.com>
2025-06-22 06:25:10 -0300

    Att: Aumento dos pods

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -27,7 +27,7 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "default-pool"
   location   = "us-central1-a"
   cluster    = google_container_cluster.primary.name
-  node_count = 2
+  node_count = 3
 
   node_config {
     machine_type = "e2-small"
